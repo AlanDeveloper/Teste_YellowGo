3d20f8f8606f96d88f15b35d45a3688abd7d0131
AlanDeveloper <alanssantos32@gmail.com>
2023-01-11 05:00:40 -0300

    Finalizado exercicio 3

--- exercicio3.php
+++ exercicio3.php
@@ -1,28 +1,5 @@
 <?php
 
-// EXEMPLO
-//     A   B   C   D
-// A   -   -   -   -
-// B   B   -   -   D
-// C   C   C   -   -
-// D   D   -   D   -
-
-// Caminhos possíveis A-D: [A, D], [A, C, D], [A, B, D], [A, B, C, D]
-
-$matrix = [
-    ['-', '-', '-', '-'],
-    ['B', '-', '-', 'D'],
-    ['C', 'C', '-', '-'],
-    ['D', '-', 'D', '-'],
-];
-$colunas = array(
-    'A' => 0,
-    'B' => 1,
-    'C' => 2,
-    'D' => 3,
-);
-$destino = 'D';
-
 function meuCount($array)
 {
     $i = 0;
@@ -36,3 +13,72 @@ function meuCount($array)
     }
     return $i;
 }
+
+
+$ligacoes = array(
+    'A-B',
+    'B-C',
+    'C-E',
+    'B-E',
+    'B-D',
+    'A-D',
+    'D-F',
+    'D-E',
+    'E-F',
+    'E-G',
+    'F-G'
+);
+
+function montar_caminho($ligacoes, $destino = 'G', $letra = 'A') {
+    $caminhos = array();
+    for ($i = 0; $i < meuCount($ligacoes); $i++) {
+
+        if ($ligacoes[$i][0] == $letra) {
+
+            if ($ligacoes[$i][2] == $destino) {
+                $caminhos[] = array(
+                    'antes' => $ligacoes[$i][0],
+                    'depois' => $ligacoes[$i][2],
+                );
+            } else {
+                $caminhos[] = array(
+                    'antes' => $ligacoes[$i][0],
+                    'depois' => montar_caminho($ligacoes, $destino, $ligacoes[$i][2])
+                );
+            }
+        }
+    }
+    return $caminhos;
+}
+
+function montar_array($ligacoes, $destino = 'G', $letra = 'A') {
+    $todos_caminhos = array();
+
+    for ($i = 0; $i < meuCount($ligacoes); $i++) {
+
+        if ($ligacoes[$i][0] == $letra) {
+
+            if ($ligacoes[$i][2] == $destino) {
+                $todos_caminhos[] = array(
+                    'antes' => $ligacoes[$i][0],
+                    'depois' => $ligacoes[$i][2],
+                );
+            } else {
+                $todos_caminhos[] = array(
+                    'antes' => $ligacoes[$i][0],
+                    'depois' =>  montar_caminho($ligacoes, $destino, $ligacoes[$i][2])
+                );
+            }
+        }
+    }
+
+    return $todos_caminhos;
+}
+
+$todos_caminhos = montar_array($ligacoes);
+print_r($todos_caminhos);
+
+## Para ler o print, primeiro ler todas colunas "antes" para depois ler a "depois"
+## Ex: antes, antes, antes, antes, depois
+## Ex: antes, antes, antes, depois, depois
+## Formara os caminhos possíveis
